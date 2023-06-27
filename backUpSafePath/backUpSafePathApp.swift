bb51f1e66c9b69786c9894f141d12ff42b9cb35c
Tess <tessaghuang@gmail.com>
2023-06-27 10:07:37 -0400

    Initial Commit

---
+++ backUpSafePath/backUpSafePathApp.swift
@@ -0,0 +1,17 @@
+//
+//  backUpSafePathApp.swift
+//  backUpSafePath
+//
+//  Created by Scholar on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct backUpSafePathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
